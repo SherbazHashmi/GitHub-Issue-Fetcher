333a82086dc17adea9bcd163dff3b683926be2c5
Sherbaz <sherbaz@underwolfstudios.com>
2018-12-09 15:33:26 +1100

    Added github-url into config and added better error checking to process function which handles errors with fetch

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,5 +1,6 @@
 defmodule Issues.GithubIssues do
   @user_agent [{"User-agent","Elixir sherbaz.hashmi@gmail.com"}]
+  @github_url Application.get_env(:issues, :github_url)
   def fetch(user, project) do
     issues_url(user,project)
     |> HTTPoison.get(@user_agent)
@@ -9,7 +10,7 @@ defmodule Issues.GithubIssues do
     Generates Issues URL Based on Fetch Parameters
     """
     def issues_url(user,project) do
-      "https://api.github.com/repos/#{user}/#{project}/issues"
+      "#{@github_url}/repos/#{user}/#{project}/issues"
     end
 
   @doc """
@@ -18,12 +19,14 @@ defmodule Issues.GithubIssues do
     and body of response.
     """
 
-    def handle_response({_,%{status_code: status_code, body: body}}) do
-      {
-        status_code |> check_for_error(),
-      body |> Poison.Parser.parse!()
-      }
-    end
+  def handle_response({_,%{status_code: status_code, body: body}}) do
+    {
+      status_code |> check_for_error(),
+    body |> Poison.Parser.parse!()
+    }
+  end
+
+
 
   defp check_for_error(200), do: :ok
   defp check_for_error(_), do: :error
